1c2b143a9ccba7ad78ee4a6870ac812d2a6d3115
Kate Grossenbacher <kate.grossenbacher@gmail.com>
2024-01-16 14:58:18 +0100

    1.3 Commit

--- monthly_precipitation.py
+++ monthly_precipitation.py
@@ -4,12 +4,26 @@ with open('precipitation.json', encoding='utf-8') as file:
     precipitation = json.load(file)
 print(precipitation)
 
-
-for dictionary in range(len(precipitation)):
-    print(precipitation[dictionary])
-
 precipitation_list = []
 for x in precipitation:
     if "GHCND:US1WAKG0038" == x['station']:
         precipitation_list.append(x)
-print (precipitation_list)
+
+for y in precipitation_list:
+    full_date = y['date']
+    year, month, day = map(int, full_date.split('-'))
+    y['year'] = year
+    y['month'] = month
+    y['day'] = day 
+print(precipitation_list)
+
+month_list = []
+months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+for y in months:
+    sum = 0 
+    for input in precipitation_list: 
+        if y == input['month']: 
+            sum += input['value']
+    month_list.append(sum)
+
+print(month_list)
