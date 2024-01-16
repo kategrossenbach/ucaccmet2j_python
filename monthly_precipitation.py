a3da9793f15a0bba47db227eb7c6977a624b7502
Kate Grossenbacher <kate.grossenbacher@gmail.com>
2024-01-16 12:36:35 +0100

    1.2

--- monthly_precipitation.py
+++ monthly_precipitation.py
@@ -1,4 +1,15 @@
-with open('precipitation.json', encoding='utf-8') as file: 
-    precipitation = file.read()
+import json
 
+with open('precipitation.json', encoding='utf-8') as file: 
+    precipitation = json.load(file)
 print(precipitation)
+
+
+for dictionary in range(len(precipitation)):
+    print(precipitation[dictionary])
+
+precipitation_list = []
+for x in precipitation:
+    if "GHCND:US1WAKG0038" == x['station']:
+        precipitation_list.append(x)
+print (precipitation_list)
